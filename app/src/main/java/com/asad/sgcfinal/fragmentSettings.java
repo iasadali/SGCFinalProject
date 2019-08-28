f7d9b513972fa90804fb5c7b98173fbc9f91e5bf
Asad <iasadali18@gmail.com>
2019-08-28 10:32:31 +0500

    Initial commit

---
+++ app/src/main/java/com/asad/sgcfinal/fragmentSettings.java
@@ -0,0 +1,23 @@
+package com.asad.sgcfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmentSettings extends Fragment
+{
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.activity_fragment_settings, null);
+    }
+
+}
